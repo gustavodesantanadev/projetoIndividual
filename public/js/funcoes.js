838ef57ca5dabbdbfc5ab8ce1e3dc51dfd0c4e1c
gustavodesantanadev <gustavo.lima@sptech.school>
2022-11-28 22:13:59 -0300

    Revert "Finalização de todo BackEnd"
    This reverts commit ab6f94541f55727c3109eb70bdec3829bd8bd839.

--- public/js/funcoes.js
+++ public/js/funcoes.js
@@ -5,11 +5,11 @@ function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
 
-    var nomeUsuario = document.getElementById("nomeUsuario");
+    var b_usuario = document.getElementById("b_usuario");
 
     if (email != null && nome != null) {
         // window.alert(`Seja bem-vindo, ${nome}!`);
-        nomeUsuario.innerHTML = nome;
+        b_usuario.innerHTML = nome;
 
         // finalizarAguardar();
     } else {
