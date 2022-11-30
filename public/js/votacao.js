7d22952933562fcd3a803562d8ad9426df25741f
gustavodesantanadev <gustavo.lima@sptech.school>
2022-11-29 21:59:58 -0300

    Votação finalizada

--- public/js/votacao.js
+++ public/js/votacao.js
@@ -1,14 +1,15 @@
 function exibirChefes(){
     const chefeArray = ["batedorMusgo","cavaleiroDaColmeia","colecionador","defensorDoEsterco","falsoCavaleiro","flukemarm","gorb","grimm","guardiaoCristalizado","guerreiroDasAlmas","hornet","hu","incubador","lordesLouvaDeus","maeMosca","marmu","mestreDasAlmas","nosk","oro","oro&mato","receptaculoQuebrado","reiVegemosca","sheo","xero"];
+    const nomeChefe = ["Imenso Batetor Musgoso","Cavaleiro da Colmeia"," O Colecionador","Defensor do Esterco", "Falso Cavaleiro", "Flukemarm","Gorb","Grimm","Guardião Cristalizado", "Guerreiro das Almas"," Hornet", "Hu", "Mawlek Incubador", "Lordes Louva Deus", "Mãe Mosca","Marmu","Mestre das Almas", "Nosk", "Mestre do Ferrão Oro", "Irmãos Oro & Mato", "Receptáculo Quebrado", "Rei Vengemosca","Mestre da Pintura Sheo", "Xero" ]
     const divTodosChefes = document.querySelector(".todosChefes");
-    
+
     for(var i = 0; i < chefeArray.length; i++){
-        divTodosChefes.innerHTML +=`
+      divTodosChefes.innerHTML +=`
       <div class="chefes">
             <img class="imgChefes" src="assets/chefes/${chefeArray[i].toLowerCase()}.jpg" alt="${chefeArray[i]}">
-            <span class="chefe-legenda">${chefeArray[i]}</span>
-            <span class="idChefe">${i + 1}</span>
-      </div>`
+            <span class="chefe-legenda">${nomeChefe[i]}</span>
+      </div>
+      `
     }
   
   }
