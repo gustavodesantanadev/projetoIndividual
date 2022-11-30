79c5870644bbebba33735e66f4c98e74fcb78376
gustavodesantanadev <gustavo.lima@sptech.school>
2022-11-29 23:28:31 -0300

    Versão quase final do Ranking

--- public/js/votacao.js
+++ public/js/votacao.js
@@ -1,12 +1,15 @@
 function exibirChefes(){
-    const chefeArray = ["batedorMusgo","cavaleiroDaColmeia","colecionador","defensorDoEsterco","falsoCavaleiro","flukemarm","gorb","grimm","guardiaoCristalizado","guerreiroDasAlmas","hornet","hu","incubador","lordesLouvaDeus","maeMosca","marmu","mestreDasAlmas","nosk","oro","oro&mato","receptaculoQuebrado","reiVegemosca","sheo","xero"];
-    const nomeChefe = ["Imenso Batetor Musgoso","Cavaleiro da Colmeia"," O Colecionador","Defensor do Esterco", "Falso Cavaleiro", "Flukemarm","Gorb","Grimm","Guardião Cristalizado", "Guerreiro das Almas"," Hornet", "Hu", "Mawlek Incubador", "Lordes Louva Deus", "Mãe Mosca","Marmu","Mestre das Almas", "Nosk", "Mestre do Ferrão Oro", "Irmãos Oro & Mato", "Receptáculo Quebrado", "Rei Vengemosca","Mestre da Pintura Sheo", "Xero" ]
+    const nomeChefe = ["Imenso Batedor Musgoso","Cavaleiro da Colmeia","O Colecionador",
+    "Defensor do Esterco", "Falso Cavaleiro", "Flukemarm","Gorb","Grimm","Guardião Cristalizado", 
+    "Guerreiro das Almas","Hornet", "Hu", "Mawlek Incubador", "Lordes Louva Deus", "Mãe Mosca","Marmu","Mestre das Almas", 
+    "Nosk", "Mestre do Ferrão Oro","Irmãos Oro & Mato", "Receptáculo Quebrado", "Rei Vengemosca","Mestre da Pintura Sheo", "Xero"]
+
     const divTodosChefes = document.querySelector(".todosChefes");
 
-    for(var i = 0; i < chefeArray.length; i++){
+    for(var i = 0; i < nomeChefe.length; i++){
       divTodosChefes.innerHTML +=`
       <div class="chefes">
-            <img class="imgChefes" src="assets/chefes/${chefeArray[i].toLowerCase()}.jpg" alt="${chefeArray[i]}">
+            <img class="imgChefes" src="assets/chefes/${nomeChefe[i]}.jpg" alt="${nomeChefe[i]}">
             <span class="chefe-legenda">${nomeChefe[i]}</span>
       </div>
       `
